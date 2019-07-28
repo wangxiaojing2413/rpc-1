02c985c9aac899b47b6fcdffae2d8251a828ede6
wangxiaojing2413 <18752031024@163.com>
2019-07-28 11:04:22 +0800

    多服务注册测试

---
+++ rpc-provider-test/src/main/java/com/kangyonggan/rpc/service/NameService.java
@@ -0,0 +1,9 @@
+package com.kangyonggan.rpc.service;
+
+/**
+ * @author caolidong
+ * @date 17/8/21.
+ */
+public interface NameService {
+    String getName();
+}
---
+++ rpc-provider-test/src/main/java/com/kangyonggan/rpc/service/impl/NameServiceImpl.java
@@ -0,0 +1,15 @@
+package com.kangyonggan.rpc.service.impl;
+
+import com.kangyonggan.rpc.service.NameService;
+
+/**
+ * @author caolidong
+ * @date 17/8/21.
+ */
+public class NameServiceImpl implements NameService {
+
+    @Override
+    public String getName() {
+        return "dong";
+    }
+}
